e58133571804e4df213ada48c74c3671859e607f
migueltmsp <migueltm.sp@gmail.com>
2021-07-25 04:59:24 +0200

    First model (doctor) created with Sequelize

---
+++ models/doctor.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Doctor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Doctor.init({
+    nombreCompleto: DataTypes.STRING,
+    numeroColegiado: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Doctor',
+  });
+  return Doctor;
+};
