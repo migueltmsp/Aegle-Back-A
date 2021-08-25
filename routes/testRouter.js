2ee2f86482e97e87573f733cb5ee4ca2c909f91e
migueltmsp <migueltm.sp@gmail.com>
2021-08-25 11:45:13 +0200

    Applied best practices

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -32,7 +32,6 @@ router.post('/id', async (req, res)=> {
     try {
         let id = req.body.id;
 
-        console.log("Id is",req.body.id)
         let answer = await testController.testId(id);
         console.log(answer[0].dataValues)
         answer[0].dataValues.estado = JSON.parse(answer[0].dataValues.estado)
@@ -50,14 +49,8 @@ router.post('/autocomplete', async (req, res)=> {
     try {
         let dni = req.body.dni;
 
-     /*    console.log("Id es",req.body.id)
-        console.log("Edad es",req.body.edad) */
-
         let answer = await testController.autocomplete(dni);
-       /*  console.log("DATAVALUES ES", answer[0].dataValues) */
 
-        /* answer[0].dataValues.edad = JSON.parse(answer[0].dataValues.edad) */
-        console.log("AHORAAAAAAAAAAAAAAAAAAAAAAAAAAA",answer[0].dataValues.edad)
         res.json(answer[0].dataValues)
     } catch (err) {
         return res.status(500).json({
@@ -72,11 +65,7 @@ router.patch('/patchCase', async (req, res)=> {
         let doctorId = req.body.doctorId;
         let estado = req.body.estado;
 
-        console.log("Id is",req.body.id)
-        console.log("doctorId is",req.body.doctorId)
-
         const caso = (await testController.patchCase(id, doctorId))
-        console.log(caso.dataValues)
 
         caso.doctorId = doctorId;
         caso.estado = estado;
@@ -98,10 +87,8 @@ router.patch('/testReceive', async (req, res)=> {
 
         let informacion = req.body;
         let id = 1
-        console.log(informacion)
 
         const caso = (await testController.patchCase(id))
-        console.log(caso.dataValues)
 
         caso.datosActuacion = JSON.stringify(informacion.datosActuacion);
         caso.datosPersonales = JSON.stringify(informacion.datosPersonales);
@@ -109,8 +96,6 @@ router.patch('/testReceive', async (req, res)=> {
         caso.diagTrat = JSON.stringify(informacion.diagTrat);
         caso.manejoResolucion = JSON.stringify(informacion.manejoResolucion);
 
-
-        /* console.log(caso.dataValues) */
         
         await caso.save();
 
