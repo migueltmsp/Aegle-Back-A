296a252b4d7551fcf5d21cebc3c56f4dfd81a060
migueltmsp <migueltm.sp@gmail.com>
2021-09-04 06:39:01 +0200

    Changed port to environment variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 6000;
 const router = require('./router');
 const db = require('./db.js');
 const cors = require('cors');
@@ -8,18 +8,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-
 // Middleware
 app.use(cors());
 app.use(express.json()); 
 app.use(router);
 
 
- 
+
 //Connecting to the database
 db.then(()=>{
     //Starting server
     
-        app.listen( port, ()=> console.log(`Server (Node) running on http://localhost:${port}`));
+        app.listen(process.env.PORT || port, ()=> console.log(`Server (Node) running on http://localhost:${port}`));
     })
     .catch((err)=> console.log(err.message));
